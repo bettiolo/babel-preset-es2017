4f5cc46eb59d14c634f377164534f3ac2788cca8
Marco Bettiolo <marco@bettiolo.it>
2016-07-29 11:48:09 -0700

    Update index.js
    Fixes: https://github.com/bettiolo/babel-preset-es2017/issues/17

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 module.exports = {
   plugins: [
+    require('babel-plugin-transform-es2015-modules-commonjs'), // This should be optional to allow tree shaking in webpack
+    
     // ES2015 w/ Node 5
     require('babel-plugin-syntax-object-rest-spread'),
     require('babel-plugin-transform-es2015-destructuring'),
-    require('babel-plugin-transform-es2015-modules-commonjs'),
     require('babel-plugin-transform-es2015-parameters'),
     require('babel-plugin-transform-es2015-sticky-regex'),
     require('babel-plugin-transform-es2015-unicode-regex'),
     require('babel-plugin-transform-strict-mode'),
-    require('babel-plugin-transform-flow-strip-types'),
     require('babel-plugin-transform-object-rest-spread'),
 
     // ES2016 w/ Node 5
